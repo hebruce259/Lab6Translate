5c2a4daccf4a3aab2b06e7d176923dfa5b1cc640
hebruce259 <saundrabruce@hotmail.com>
2016-10-31 23:55:55 -0400

    Translate to Pig Latin

--- Lab6TranslateToEnglish/Lab6TranslateToEnglish/Program.cs
+++ Lab6TranslateToEnglish/Lab6TranslateToEnglish/Program.cs
@@ -10,15 +10,36 @@ namespace Lab6TranslateToEnglish
     {
         static void Main(string[] args)
         {
+         //   const string isVowel =  new string ("aeiou");
             Console.WriteLine("Welcome to Pig Latin Translator!");
             
-            //Translate after user input here.
+            //User input here.
             Console.Write("Enter word to be translated:");
+
+            //Convert to Lower Case.
             string word = Console.ReadLine();
             string lowerCaseWord = word.ToLower();
+            string wordOut ="";
+            
             Console.WriteLine("{0}", lowerCaseWord);
-            //Convert to lowercase here.
-          //  Console.WriteLine(word);
+
+            //if the first letter is a vowel attach "way" to the ending.
+            if (lowerCaseWord.StartsWith("a") || lowerCaseWord.StartsWith("e") || lowerCaseWord.StartsWith("i") || lowerCaseWord.StartsWith("o") || lowerCaseWord.StartsWith("u"))
+            {
+                Console.WriteLine(lowerCaseWord + "way");
+            }
+            else
+            //It's a Consonant chop the beginning letter off and move to the back adding "ay" to the ending.
+            {
+                for (int i = 1; i < lowerCaseWord.Length; i++)
+                {
+                    wordOut = wordOut + lowerCaseWord[i];
+                }
+                wordOut = wordOut + lowerCaseWord[0];
+                Console.WriteLine(wordOut + "ay");
+            }
+
+            
         }
     }
 }
